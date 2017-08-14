3bd222280345c19b2e32ec5fd2fb3ec800de5e90
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-14 08:32:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Extra gmail features to better help lichess users
+
+Install as an unpacked chrome extension.
+
+### Commands:
+
+`Ctrl+i` copies the sender email to clipboard
---
+++ src/inject/inject.js
@@ -0,0 +1,40 @@
+chrome.extension.sendMessage({}, function(response) {
+  document.body.addEventListener('keypress', function(e) {
+    if (e.keyCode === 9) {
+      var email = document.querySelector('img.ajn[jid]').getAttribute('jid');
+      copyTextToClipboard(email);
+      console.log('copied', email);
+    }
+  });
+});
+
+// <https://stackoverflow.com/a/17644403>
+function copyTextToClipboard(html) {
+
+  var tmpNode = document.createElement('div')
+  tmpNode.innerHTML = html
+  document.body.appendChild(tmpNode)
+
+  // Back up previous selection
+  var selection = window.getSelection()
+  var backupRange
+  if (selection.rangeCount)
+    backupRange = selection.getRangeAt(0).cloneRange()
+
+  // Copy the contents
+  var copyFrom = document.createRange()
+  copyFrom.selectNodeContents(tmpNode)
+  selection.removeAllRanges()
+  selection.addRange(copyFrom)
+  document.execCommand('copy')
+
+  // Clean-up
+  tmpNode.parentNode.removeChild(tmpNode)
+
+  // Restore selection
+  selection = window.getSelection()
+  selection.removeAllRanges()
+  if (backupRange)
+    selection.addRange(backupRange)
+
+}

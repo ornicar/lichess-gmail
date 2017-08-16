9f79fd65b2b261908a03e2676546075b5306ab71
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-16 08:59:32 -0500

    reformat

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -11,30 +11,27 @@ chrome.extension.sendMessage({}, function(response) {
 // <https://stackoverflow.com/a/17644403>
 function copyTextToClipboard(html) {
 
-  var tmpNode = document.createElement('div')
-  tmpNode.innerHTML = html
-  document.body.appendChild(tmpNode)
+  var tmpNode = document.createElement('div');
+  tmpNode.innerHTML = html;
+  document.body.appendChild(tmpNode);
 
   // Back up previous selection
-  var selection = window.getSelection()
-  var backupRange
-  if (selection.rangeCount)
-    backupRange = selection.getRangeAt(0).cloneRange()
+  var selection = window.getSelection();
+  var backupRange;
+  if (selection.rangeCount) backupRange = selection.getRangeAt(0).cloneRange();
 
   // Copy the contents
-  var copyFrom = document.createRange()
-  copyFrom.selectNodeContents(tmpNode)
-  selection.removeAllRanges()
-  selection.addRange(copyFrom)
-  document.execCommand('copy')
+  var copyFrom = document.createRange();
+  copyFrom.selectNodeContents(tmpNode);
+  selection.removeAllRanges();
+  selection.addRange(copyFrom);
+  document.execCommand('copy');
 
   // Clean-up
-  tmpNode.parentNode.removeChild(tmpNode)
+  tmpNode.parentNode.removeChild(tmpNode);
 
   // Restore selection
-  selection = window.getSelection()
-  selection.removeAllRanges()
-  if (backupRange)
-    selection.addRange(backupRange)
-
+  selection = window.getSelection();
+  selection.removeAllRanges();
+  if (backupRange) selection.addRange(backupRange);
 }
